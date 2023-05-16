50e323159b1c725526d823c99a9971840a83aee2
Pavlo Kikis <99750479+n3xtor@users.noreply.github.com>
2023-05-16 19:53:07 +0300

    [create] Card.cs

---
+++ Gypsy Game/Card.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Gypsy_Game
+{
+    public enum Suit
+    {
+        Diamonds, Clubs, Hearts, Spades
+    }
+
+    public enum Rank
+    {
+        Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+    }
+
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public Rank Rank { get; set; }
+
+        public Card(Suit suit, Rank rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+
+        public override string ToString()
+        {
+            return $"{Rank} of {Suit}";
+        }
+    }
+}
