dff7fa1cbda90ce86a4d59b7575460a06ac1886f
Pavlo Kikis <99750479+n3xtor@users.noreply.github.com>
2023-05-16 19:51:45 +0300

    [remove] last update

--- Gypsy Game/Program.cs
+++ Gypsy Game/Program.cs
@@ -2,33 +2,6 @@
 
 namespace Gypsy_Game
 {
-    public enum Suit
-    {
-        Diamonds, Clubs, Hearts, Spades
-    }
-
-    public enum Rank
-    {
-        Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
-    }
-
-    public class Card
-    {
-        public Suit Suit { get; set; }
-        public Rank Rank { get; set; }
-
-        public Card(Suit suit, Rank rank)
-        {
-            Suit = suit;
-            Rank = rank;
-        }
-
-        public override string ToString()
-        {
-            return $"Rank {Rank} of {Suit} suit";
-        }
-    }
-    
     class Program
     {
         static void Main(string[] args)
